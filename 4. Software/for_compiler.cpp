773985af894ca9997d74a82718262f9d3ad51724
anwar-513 <anwarullah8389@gmail.com>
2026-08-04 22:11:52 +0500

    compiler and interpreter

---
+++ 4. Software/for_compiler.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    cout << "this line will be executed after the program runs.";
+
+    int age;
+    cout << "Enter Your age--->";
+    cin >> age;
+
+    return 0;
+}
---
+++ 4. Software/for_interpretor.py
@@ -0,0 +1,4 @@
+print("Python is using an interpreter")
+print("This line runs immediately")
+name = input("Enter your name: ")
+print("Hello,", name)
